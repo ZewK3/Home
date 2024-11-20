061f46cfac9455509ddf33319604fc0a9877b107
Zew1106 <117955562+Zew1106@users.noreply.github.com>
2024-11-20 22:10:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function submitData() {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(data),
-            mode: "cors"
+            mode: "cors",
         });
         const result = await response.json();
         alert(result.message);
