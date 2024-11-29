d5988f7a28db7e1f7f4fd8ea5405a7f17ec7f74b
ZewK <117955562+ZewK3@users.noreply.github.com>
2024-11-30 01:40:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ document.getElementById("registerForm").addEventListener("submit", async functio
         if (checkResponse.status === 404) {
             // Đăng ký nếu mã nhân viên chưa tồn tại
             const registerResponse = await fetch(
-                "https://zewk.tocotoco.workers.dev/register?action=register",
+                "https://zewk.tocotoco.workers.dev?action=register",
                 {
                     method: "POST",
                     headers: {
