d8e7e64e670b0ee5161071566cabe06b40a3a1e7
ZewK <117955562+ZewK3@users.noreply.github.com>
2025-07-18 00:39:12 +0700

    Update script.js
    Signed-off-by: ZewK <117955562+ZewK3@users.noreply.github.com>

--- script.js
+++ script.js
@@ -203,7 +203,7 @@ async function handleLogin(event) {
             if (elements.loginForm.rememberMe.checked) {
                 localStorage.setItem(REMEMBER_ME_KEY, formData.loginEmployeeId);
             }
-            localStorage.setItem("loggedInUser", elements.loginForm.loginEmployeeId.value.trim());
+            localStorage.setItem("loggedInUser", JSON.stringify(formData));
             showNotification("Đăng nhập thành công!");
             
             setTimeout(() => window.location.href = "dashboard.html", 1000);
