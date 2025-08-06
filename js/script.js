07d8959b7b71da9f2dba32c0ade6e083e9f36c2c
ZewK <117955562+ZewK3@users.noreply.github.com>
2025-08-06 14:42:12 +0700

    Update script.js
    Signed-off-by: ZewK <117955562+ZewK3@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Constants
-import { CONFIG } from './config.js';
+const API_URL = "https://zewk.tocotoco.workers.dev/";
 const TOKEN_KEY = "authToken";
 const REMEMBER_ME_KEY = "rememberedEmployeeId";
 const THEME_KEY = "theme";
