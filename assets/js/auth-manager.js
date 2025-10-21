e9f2e9c88ff3b448c34a834953cb3b7777e0e570
ZewK <117955562+ZewK3@users.noreply.github.com>
2025-10-22 02:44:03 +0700

    Test Dashboard

--- assets/js/auth-manager.js
+++ assets/js/auth-manager.js
@@ -480,7 +480,7 @@ class AuthManager {
     async checkAuthentication() {
         if (!this.token) {
             console.log('No token found, redirecting to login');
-            // window.location.href = "../../index.html";
+            window.location.href = "../../index.html";
             return null;
         }
 
@@ -567,7 +567,7 @@ class AuthManager {
         }
         
         console.log('Logout completed, redirecting to login');
-        // window.location.href = "../../index.html";
+        window.location.href = "../../index.html";
     }
 }
 
